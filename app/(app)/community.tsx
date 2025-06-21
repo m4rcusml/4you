6fa102db2f4299ff9e9b4627581eaf89e079f268
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-06-21 12:33:12 -0300

    feat: adicionar configuração do Google Maps e permissões de localização

--- app/(app)/community.tsx
+++ app/(app)/community.tsx
@@ -1,12 +1,19 @@
 import { View } from 'react-native'
-import { Heart } from '@nandorojo/heroicons/24/solid'
 import { Typography } from '@/components/ui/typography'
+import Logo from '@/assets/images/logo-primary.svg'
 
 export default function Profile() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Typography className="text-2xl font-bold">Tela do botão do meio sla</Typography>
-      <Heart color='red' width={64} height={64} />
+    <View className="flex-1 p-6 gap-4 items-center justify-center">
+      <Logo className="w-32 h-32" />
+
+      <Typography className="text-center text-2xl font-bold">
+        Fique calmo, em breve alguém irá até você.
+      </Typography>
+
+      <Typography className="text-center text-lg mt-2">
+        Estamos chamando seus contatos de emergência e compartilhando sua localização.
+      </Typography>
     </View>
   )
 }
--- components/general/Map/map.tsx
+++ components/general/Map/map.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet } from 'react-native';
 
 export default function Map({ initialRegion, markers }: any) {
   return (
-    <MapView style={styles.map} initialRegion={initialRegion}>
+    <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={initialRegion}>
       {markers.map((marker: any, index: number) => (
         <Marker
           key={index}
